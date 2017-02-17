55639ff521950c6959ec48d9038d59cdd20e801b
hichemcesar24 <hichem.romdhane@esprit.tn>
2017-02-17 01:17:01 +0100

    Weather android
    Weather android

---
+++ app/src/main/java/hichem/karam/weather/Constants.java
@@ -0,0 +1,7 @@
+package hichem.karam.weather;
+
+public class Constants {
+    public static final String  DEFAULT_CITY = "Ariana";
+    public static final String  DEFAULT_LAT = "36.8665";
+    public static final String  DEFAULT_LON = "10.1647";
+}
---
+++ app/src/main/java/hichem/karam/weather/tasks/ParseResult.java
@@ -0,0 +1,3 @@
+package hichem.karam.weather.tasks;
+
+public enum ParseResult {OK, JSON_EXCEPTION, CITY_NOT_FOUND}
